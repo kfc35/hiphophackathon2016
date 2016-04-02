72919fb9a43553fe6161f4202fad8b6bbf9fd09a
Kevin CHen <chen.kevin.f@gmail.com>
2016-04-02 17:48:05 -0400

    read first file

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,25 @@
 var express = require('express');
+var path = require('path');
+var fs = require('fs');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send(readFiles());
 });
+
+function readFiles(dirname, onFileContent, onError) {
+	var fileNames = fs.readdirSync(path.join(__dirname, "../public/lyrics/kanye/college_dropout"));
+  var jsonContent;
+	fileNames.forEach(function(fileName) {
+    if (fileName === ".DS_Store") {
+      return ;
+    }
+    console.log(fileName);
+		var content = fs.readFileSync(path.join(__dirname, "../public/lyrics/kanye/college_dropout/" + fileName));
+    jsonContent = JSON.parse(content);
+	});
+  return jsonContent;
+}
 
 module.exports = router;
